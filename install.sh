b48d084a5d0e5b8d221588ac06823ec9f1bd2562
Vladimir Kamuz <v.kamuz@gmail.com>
2021-05-06 23:29:49 +0300

    Init

---
+++ install.sh
@@ -0,0 +1,35 @@
+
+#!/bin/bash
+
+echo "----- WordPress Installer -----"
+
+url='sitename.loc'
+title='sitename'
+admin_user='admin'
+admin_password='12345'
+admin_email='v.kamuz@gmail.com'
+dbname='sitename'
+dbuser='root'
+dbpass='root'
+
+wp core download --skip-content --force
+
+wp config create --dbname=$dbname --dbuser=$dbuser --dbpass=$dbpass --extra-php <<PHP
+define( 'WP_DEBUG', true );
+define( 'WP_DEBUG_LOG', true );
+define( 'WP_DEBUG_DISPLAY', false );
+define('AUTOSAVE_INTERVAL', 360000 );
+define('WP_POST_REVISIONS', false );
+PHP
+
+wp db create
+
+wp core install --url=$url --title="$title" --admin_user=$admin_user --admin_password=$admin_password --admin_email=$admin_email
+
+wp plugin install query-monitor all-in-one-wp-migration --activate
+# wp plugin install generateblocks duplicate-post
+
+wp plugin delete hello akismet
+# wp theme install generatepress --activate
+
+echo "----- Installation is complete -----"
